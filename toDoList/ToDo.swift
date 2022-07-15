89f9e03e48d9ce09145663a3014b7b0399676d26
Darshini Bhasker <bhasker.darshini@gmail.com>
2022-07-15 16:25:10 -0700

    iteration 0 done

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Scholar on 7/15/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = "OH NO THE TABLE ITS BROKEN"
+    var important = true
+    
+}
